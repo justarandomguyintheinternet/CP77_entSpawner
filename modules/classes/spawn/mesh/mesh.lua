f5c7c92ec8b24c023f8ed3d3bb013ba8c8e96143
keanuWheeze <48886873+justarandomguyintheinternet@users.noreply.github.com>
2024-05-06 23:46:05 +0200

    Mesh scaling

--- modules/classes/spawn/mesh/mesh.lua
+++ modules/classes/spawn/mesh/mesh.lua
@@ -10,7 +10,7 @@ function mesh:new()
     o.spawnDataPath = "data/spawnables/mesh/"
     o.modulePath = "mesh/mesh"
 
-    o.scale = { 1, 1, 1 }
+    o.scale = { x = 1, y = 1, z = 1 }
 
     setmetatable(o, { __index = self })
    	return o
@@ -27,8 +27,27 @@ function mesh:spawn()
         local component = entMeshComponent.new()
         component.name = "mesh"
         component.mesh = ResRef.FromString(self.spawnData)
+        component.visualScale = Vector3.new(self.scale.x, self.scale.y, self.scale.z)
         entity:AddComponent(component)
     end)
+end
+
+function mesh:save()
+    local data = spawnable.save(self)
+    data.scale = self.scale
+
+    return data
+end
+
+function mesh:updateScale()
+    local entity = self:getEntity()
+    if not entity then return end
+
+    local component = entity:FindComponentByName("mesh")
+    component.visualScale = Vector3.new(self.scale.x, self.scale.y, self.scale.z)
+
+    component:Toggle(false)
+    component:Toggle(true)
 end
 
 function mesh:draw()
@@ -37,11 +56,30 @@ function mesh:draw()
     ImGui.Spacing()
     ImGui.Separator()
     ImGui.Spacing()
+
+    ImGui.PushItemWidth(150)
+    self.scale.x, changed = ImGui.DragFloat("##xsize", self.scale.x, 0.01, -9999, 9999, "%.3f X Scale")
+    if changed then
+        self:updateScale()
+    end
+    ImGui.SameLine()
+    self.scale.y, changed = ImGui.DragFloat("##ysize", self.scale.y, 0.01, -9999, 9999, "%.3f Y Scale")
+    if changed then
+        self:updateScale()
+    end
+    ImGui.SameLine()
+    self.scale.z, changed = ImGui.DragFloat("##zsize", self.scale.z, 0.01, -9999, 9999, "%.3f Z Scale")
+    if changed then
+        self:updateScale()
+    end
+    ImGui.SameLine()
+    ImGui.PopItemWidth()
 end
 
 function mesh:export()
     local data = spawnable.export(self)
     data.type = "worldMeshNode"
+    data.scale = self.scale
     data.data = {
         mesh = {
             DepotPath = {
