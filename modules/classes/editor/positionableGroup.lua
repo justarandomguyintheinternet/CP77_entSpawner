f5758fec5dfc6c72806bee378f5bcca09af1f6a7
keanuWheeze <48886873+justarandomguyintheinternet@users.noreply.github.com>
2024-08-23 16:51:23 +0200

    Group and spawnableElement setup

--- modules/classes/editor/positionableGroup.lua
+++ modules/classes/editor/positionableGroup.lua
@@ -4,25 +4,25 @@ local settings = require("modules/utils/settings")
 local positionable = require("modules/classes/editor/positionable")
 
 ---Class for organizing multiple objects and or groups
----@class group : positionable
+---@class positionableGroup : positionable
 ---@field isUsingSpawnables boolean
-local group = setmetatable({}, { __index = positionable })
+local positionableGroup = setmetatable({}, { __index = positionable })
 
-function group:new(sUI)
+function positionableGroup:new(sUI)
 	local o = positionable.new(self, sUI)
 
 	o.name = "New Group"
-	o.modulePath = "modules/classes/editor/group"
+	o.modulePath = "modules/classes/editor/positionableGroup"
 
 	o.isUsingSpawnables = true
-	o.class = utils.combine(o.class, { "group" })
+	o.class = utils.combine(o.class, { "positionableGroup" })
 
 	setmetatable(o, { __index = self })
    	return o
 end
 
 ---@override
-function group:load(data)
+function positionableGroup:load(data)
 	positionable.load(self, data)
 
 	self.pos = utils.getVector(data.pos)
@@ -30,24 +30,26 @@ end
 
 ---Draw func if this is just a sub group
 ---@protected
-function group:drawProperties()
+function positionableGroup:drawProperties()
 	self.pos = self:getCenter()
 
 	positionable.drawProperties(self)
 end
 
-function group:getDirection(direction)
+function positionableGroup:getDirection(direction)
 	local leafs = self:getPositionableLeafs()
 	local dir = Vector4.new(0, 0, 0, 0)
 
 	for _, entry in pairs(leafs) do
-		dir = utils.addVector(dir, entry:getDirection(direction))
+		if entry.visible then
+			dir = utils.addVector(dir, entry:getDirection(direction))
+		end
 	end
 
 	return Vector4.new(dir.x / #leafs, dir.y / #leafs, dir.z / #leafs, 0)
 end
 
-function group:getCenter()
+function positionableGroup:getCenter()
 	local center = Vector4.new(0, 0, 0, 0)
 
 	local leafs = self:getPositionableLeafs()
@@ -60,13 +62,13 @@ end
 
 ---Gets all the positionable leaf objects, i.e. positionable's without childs#
 ---@return positionable[]
-function group:getPositionableLeafs()
+function positionableGroup:getPositionableLeafs()
 	local objects = {}
 
 	for _, entry in pairs(self.childs) do
 		if utils.isA(entry, "object") then
 			table.insert(objects, entry)
-		elseif utils.isA(entry, "group") then
+		elseif utils.isA(entry, "positionableGroup") then
 			objects = utils.combine(objects, entry:getPositionableLeafs())
 		end
 	end
@@ -74,12 +76,12 @@ function group:getPositionableLeafs()
 	return objects
 end
 
-function group:getPosition()
+function positionableGroup:getPosition()
 	self.pos = self:getCenter()
 	return self.pos
 end
 
-function group:setPosition(delta)
+function positionableGroup:setPosition(delta)
 	local leafs = self:getPositionableLeafs()
 
 	for _, entry in pairs(leafs) do
@@ -89,18 +91,28 @@ function group:setPosition(delta)
 	self.pos = self:getCenter()
 end
 
-function group:setRotation(delta)
+function positionableGroup:setRotation(delta)
 	self.pos = self:getCenter()
 
 	local leafs = self:getPositionableLeafs()
 
+	local deltaRotation = Quaternion.SetAxisAngle(Vector4.new(1 * delta.roll, 1 * delta.pitch, 1 * delta.yaw, 0), Deg2Rad(delta.roll * 1 + delta.pitch * 1 + delta.yaw * 1))
+
+	for _, entry in pairs(leafs) do
+		local relativePosition = utils.subVector(entry:getPosition(), self.pos)
+		relativePosition = deltaRotation:Transform(relativePosition)
+		entry:setPosition(utils.addVector(self.pos, relativePosition))
+
+		local entryRot = entry:getRotation():ToQuat()
+		entry:setRotation(Game['OperatorMultiply;QuaternionQuaternion;Quaternion'](deltaRotation, entryRot):ToEulerAngles())
+	end
 end
 
-function group:serialize()
+function positionableGroup:serialize()
 	local data = positionable.serialize(self)
 	data.pos = self:getCenter()
 
 	return data
 end
 
-return group
+return positionableGroup
---
+++ modules/classes/editor/spawnableElement.lua
@@ -0,0 +1,91 @@
+local utils = require("modules/utils/utils")
+
+local positionable = require("modules/classes/editor/positionable")
+
+---Class for organizing multiple objects and or groups
+---@class spawnableElement : positionable
+---@field spawnable spawnable
+local spawnableElement = setmetatable({}, { __index = positionable })
+
+function spawnableElement:new(sUI)
+	local o = positionable.new(self, sUI)
+
+	o.name = "New Element"
+	o.modulePath = "modules/classes/editor/spawnableElement"
+
+	o.spawnable = nil
+	o.class = utils.combine(o.class, { "spawnableElement" })
+
+	setmetatable(o, { __index = self })
+   	return o
+end
+
+---@override
+function spawnableElement:load(data)
+	positionable.load(self, data)
+
+	self.spawnable = require("modules/classes/spawn/" .. data.spawnable.modulePath):new()
+    self.spawnable.object = self
+    self.spawnable:loadSpawnData(data.spawnable, ToVector4(data.spawnable.position), ToEulerAngles(data.spawnable.rotation))
+
+	self:setVisible(self.visible)
+end
+
+function spawnableElement:setVisible(state, fromRecursive)
+	positionable.setVisible(self, state, fromRecursive)
+
+	if self.visible == false then
+		self.spawnable:despawn()
+	else
+		self.spawnable:spawn()
+	end
+end
+
+function spawnableElement:setHiddenByParent(state)
+	positionable.setHiddenByParent(self, state)
+
+	if self.hiddenByParent then
+		self.spawnable:despawn()
+	else
+		self.spawnable:spawn()
+	end
+end
+
+function spawnableElement:getDirection(direction)
+	if not self.spawnable:isSpawned() then
+		return Vector4.new(0, 0, 0, 0)
+	end
+
+	if direction == "forward" then
+		return self.spawnable:getEntity():GetWorldForward()
+	elseif direction == "right" then
+		return self.spawnable:getEntity():GetWorldRight()
+	elseif direction == "up" then
+		return self.spawnable:getEntity():GetWorldUp()
+	end
+end
+
+function spawnableElement:setPosition(delta)
+	self.pos = utils.addVector(self.pos, delta)
+	self.spawnable.position = self.pos
+
+	if not self.spawnable:isSpawned() then return end
+	self.spawnable:update()
+end
+
+function spawnableElement:setRotation(delta)
+	self.rot = utils.addEuler(self.rot, delta)
+	self.spawnable.rotation = self.rot
+
+	if not self.spawnable:isSpawned() then return end
+	self.spawnable:update()
+end
+
+function spawnableElement:serialize()
+	local data = positionable.serialize(self)
+	data.spawnable = self.spawnable:save()
+
+	return data
+end
+
+return spawnableElement
