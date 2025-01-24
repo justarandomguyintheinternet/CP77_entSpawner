ab53725148a007c0a625966072f66bb014011d36
keanuWheeze <48886873+justarandomguyintheinternet@users.noreply.github.com>
2025-01-24 14:21:50 +0100

    Time periods naming fix

--- modules/classes/spawn/ai/communityArea.lua
+++ modules/classes/spawn/ai/communityArea.lua
@@ -24,7 +24,36 @@ function community:new()
     o.previewColor = "palegreen"
 
     o.entries = {}
-    o.periodEnums = utils.enumTable("communityECommunitySpawnTime")
+    o.periodEnums = {
+        "Morning",
+        "Day",
+        "Evening",
+        "Night",
+        "Midnight",
+        "1:00 AM",
+        "2:00 AM",
+        "3:00 AM",
+        "4:00 AM",
+        "5:00 AM",
+        "6:00 AM",
+        "7:00 AM",
+        "8:00 AM",
+        "9:00 AM",
+        "10:00 AM",
+        "11:00 AM",
+        "Noon",
+        "1:00 PM",
+        "2:00 PM",
+        "3:00 PM",
+        "4:00 PM",
+        "5:00 PM",
+        "6:00 PM",
+        "7:00 PM",
+        "8:00 PM",
+        "9:00 PM",
+        "10:00 PM",
+        "11:00 PM"
+    }
 
     setmetatable(o, { __index = self })
    	return o
